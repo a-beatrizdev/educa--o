7e68c20cfa3c378c062c2e5f4b68c57cdf147224
Alana <alanabeatriztobias@gmail.com>
2026-04-10 16:12:21 -0300

    feat: cria funções de inserção e popula o banco

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, Float, String, ForeignKey
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
 Base = declarative_base()
@@ -11,24 +11,60 @@ class Professor(Base):
     materia = Column(String(100), nullable=False)
     salario = Column(Integer, nullable=False)
 
-    aulas = relationship("Aula", back_populates="professor")
+    aulas = relationship("Aula", back_populates="professores")
 
-    def __init__(self, nome):
-        self.nome = nome
+    def __repr__(self):
+        return f"Professor: id={self.id} - nome={self.nome} - materia={self.materia} - salario={self.salario}"
 
-        def __repr__(self):
-            return f"Professor: id={self.id} - nome={self.nome}"
-
-class Aula(Professor):
+class Aula(Base):
     __tablename__ = "aulas"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String(100), nullable=False)
     duracao = Column(Integer, nullable=False)
-    alunos_presentes = Column(Integer, nullable=False)
+    alunos_presentes = Column(Float, nullable=False)
 
-    professor = Column(Integer, ForeignKey ("professores.id"))
+    professores_id = Column(Integer, ForeignKey("professores.id"))
+    professores = relationship("Professor", back_populates="aulas")
 
     def __repr__(self):
-        return f"id={self.id} - nome={self.nome}"
+        return f"id={self.id} - nome={self.nome} - duracao={self.duracao} - alunos_presentes={self.alunos_presentes}"
 
+engine = create_engine("sqlite:///educacao.db")
+
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+
+def cadastrar_professor():
+    with Session() as session:
+        try:
+            nome_professor = input("Digite o nome do professor: ").capitalize()
+            materia = input("Digite a matéria do professor: ").capitalize()
+            salario = input("Digite o salário do professor: ").capitalize()
+            professor = Professor(nome=nome_professor, materia=materia, salario=salario)
+
+            session.add(professor)
+            session.commit()
+            print(f"Professor cadastrado com sucesso!")
+        except Exception as erro:
+            session.rollback()
+            print(f"Ocorreu um erro {erro}")
+
+# cadastrar_professor()
+        
+def cadastrar_aula():
+    with Session() as session:
+        try:
+            nome = input("Digite o nome da aula: ").capitalize()
+            duracao = input("Digite o tempo de duração da aula: ").capitalize()
+            alunos_presentes = input("Digite a quantidade de alunos presentes: ").capitalize()
+            aulas = Aula(nome=nome, duracao=duracao, alunos_presentes=alunos_presentes)
+
+            session.add(aulas)
+            session.commit()
+            print(f"Aula cadastrada com sucesso!")
+        except Exception as erro:
+            session.rollback()
+            print(f"Ocorreu um erro {erro}")
+
+cadastrar_aula()
